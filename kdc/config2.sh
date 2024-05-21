b2b1541ce4c6ae2d74a9f48828f040d266949a3b
LakshmiSagarNS <nslakshmisagar12@gmail.com>
2024-05-21 13:44:49 +0530

    services and user principals are written into sh scripts

--- kdc/config2.sh
+++ kdc/config2.sh
@@ -3,7 +3,7 @@
 # Deafult values
 REALM_NAME="${REALM_NAME-EXAMPLE.COM}"
 DOMAIN_NAME="${DOMAIN_NAME-example.com}"
-KADMIN_PASS="${KADMIN_PASS-Secure_Password}"
+KADMIN_PASS="${KADMIN_PASS-root}"
 MASTER_PASS="${MASTER_PASS-Master_Password}"
 
 # Copying krb5 conf file
@@ -45,7 +45,34 @@ ${KADMIN_PASS}
 EOL
 
 # Start services
-kadmind 
-krb5kdc
+#kadmind 
+#krb5kdc
 
+#tail -f /var/log/krb5kdc.log
+
+#! /bin/bash
+
+/usr/sbin/kadmin.local -q "addprinc -randkey hdfs/namenode"
+/usr/sbin/kadmin.local -q "addprinc -randkey hdfs/datanode"
+/usr/sbin/kadmin.local -q "addprinc -randkey hdfs/resourcemanager"
+/usr/sbin/kadmin.local -q "addprinc -randkey hdfs/nodemanager"
+/usr/sbin/kadmin.local -q "addprinc -randkey yarn/resourcemanager"
+/usr/sbin/kadmin.local -q "addprinc -randkey yarn/nodemanager"
+/usr/sbin/kadmin.local -q "addprinc -randkey hadoop"
+
+/usr/sbin/kadmin.local -q "ktadd -k /var/keytabs/hdfs.keytab hdfs/namenode"
+/usr/sbin/kadmin.local -q "ktadd -k /var/keytabs/hdfs.keytab hdfs/datanode"
+/usr/sbin/kadmin.local -q "ktadd -k /var/keytabs/hdfs.keytab hdfs/resourcemanager"
+/usr/sbin/kadmin.local -q "ktadd -k /var/keytabs/hdfs.keytab hdfs/nodemanager"
+/usr/sbin/kadmin.local -q "ktadd -k /var/keytabs/hdfs.keytab yarn/nodemanager"
+/usr/sbin/kadmin.local -q "ktadd -k /var/keytabs/hdfs.keytab yarn/resourcemanager"
+/usr/sbin/kadmin.local -q "ktadd -k /var/keytabs/hdfs.keytab hadoop"
+
+kadmin.local -q "addprinc -pw hdfs hdfs"
+kadmin.local -q "addprinc -pw sagar sagar"
+
+chmod 777 /var/keytabs/hdfs.keytab
+
+
+krb5kdc -n
 tail -f /var/log/krb5kdc.log
--- kdc/namenode-start.sh
+++
@@ -1,8 +0,0 @@
-until kinit -kt /var/keytabs/hdfs.keytab hdfs/namenode; do sleep 2; done
-
-echo "KDC is up and ready to go... starting up"
-
-kdestroy
-
-hdfs namenode -format
-hdfs namenode
